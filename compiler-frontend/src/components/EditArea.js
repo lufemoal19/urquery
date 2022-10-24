8d52a6df1e10007f2dc2454f1106757066f1c30d
lufemoal19 <lufemoal19@gmail.com>
2022-10-23 20:53:27 -0600

    update css and component edit area

--- compiler-frontend/src/components/EditArea.js
+++ compiler-frontend/src/components/EditArea.js
@@ -5,7 +5,6 @@ export const EditArea = () => {
     <div className='box'>
         <h4>Edit Area</h4>
         <section className='box__text__area'>
-    
             <textarea placeholder='CODE HERE' cols='50' rows='20'>
                 
             </textarea>
